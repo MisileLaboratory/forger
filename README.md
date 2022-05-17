c15fbb61970aa8fbaf915deba98b1f46546a53f5
MisileLaboratory <misileminecord@gmail.com>
2022-05-18 01:04:32 +0900

    init 2

--- README.md
+++ README.md
@@ -1,3 +1,6 @@
-# Base-repository
+# Forger
+
 ![Rust](https://img.shields.io/badge/language-rust-1976d2?style=for-the-badge&logo=rust)
 ![License](https://img.shields.io/badge/license-misilelab-green?style=for-the-badge)
+
+Install fabric, quilt with cli! (Forge is adfly link, so I can't install it directly)
--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,28 @@
-struct Parser {
+use serde::Deserialize;
+use clap::Parser;
+
+/// Download fabric, quilt server or client with cli.
+#[derive(Parser, Debug)]
+#[clap(author, version, about, long_about = None)]
+struct ForgerParser {
+  /// Modloader that will be download.
+  #[clap(long)]
   modloader: String,
-  version: String
+
+  /// Minecraft version that will be download.
+  #[clap(long)]
+  version: String,
+
+  /// Server or client version?
+  #[clap(long)]
+  server: Option<bool>
 }
 
 fn main() {
-  
+  let args = ForgerParser::parse();
+}
+
+// https://maven.fabricmc.net/net/fabricmc/fabric-installer/maven-metadata.xml
+fn install_fabric() {
+
 }
