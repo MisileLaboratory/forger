95226d2695b20b689df48f4c8c45f684191adffe
MisileLaboratory <misileminecord@gmail.com>
2022-05-19 01:18:00 +0900

    feat: :sparkles: find latest version and prepare download

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,12 @@
-use serde::Deserialize;
 use clap::Parser;
+
+use quick_xml;
+
+use serde::Deserialize;
+
+use anyhow::Error;
+
+use std::{path::Path, fs::remove_file};
 
 /// Download fabric, quilt server or client with cli.
 #[derive(Parser, Debug)]
@@ -18,11 +25,49 @@ struct ForgerParser {
   server: Option<bool>
 }
 
-fn main() {
+#[derive(Deserialize)]
+struct ModLoaderVersion {
+  metadata: XMLMetadata
+}
+
+#[derive(Deserialize)]
+struct XMLMetadata {
+  groupId: String,
+  artifactId: String,
+  versioning: Versions
+}
+
+#[derive(Deserialize)]
+struct Versions {
+  #[serde(rename = "latest")]
+  latest_version: String,
+  release: String,
+  versions: Vec<String>,
+  #[serde(rename = "lastUpdated")]
+  last_updated: String
+}
+
+#[tokio::main]
+async fn main() {
   let args = ForgerParser::parse();
 }
 
 // https://maven.fabricmc.net/net/fabricmc/fabric-installer/maven-metadata.xml
-fn install_fabric() {
-
+async fn install_fabric(server: bool) -> Result<(), Error> {
+  let serverstring = if server {
+    "-server"
+  } else {
+    ""
+  };
+  let response = reqwest::get("https://maven.fabricmc.net/net/fabricmc/fabric-installer/maven-metadata.xml").await?.text().await?;
+  let version: ModLoaderVersion = quick_xml::de::from_str(&response)?;
+  let version = version.metadata.versioning.latest_version;
+  drop(response);
+  let filename = format!("fabric-installer-{}{}.jar", version, serverstring);
+  if Path::new(filename).exists() {
+    remove_file(filename)?;
+  }
+  let mut response = reqwest::get(format!("https://maven.fabricmc.net/net/fabricmc/fabric-installer/{}/{}", version, filename)).await?;
+  // let mut file =
+  Ok(())
 }
